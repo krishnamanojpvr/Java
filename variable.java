6817c0e03faca73a9c19ea115c5a8398883ac197
Krishna Manoj PVR <pvrkmsbunny@gmail.com>
2024-03-15 21:18:36 +0530

    java_basics

---
+++ variable.java
@@ -0,0 +1,17 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+        fun1(args);
+    }
+
+    public static void fun1(String[] args) {
+        int a = 10;
+        float f = a;
+        System.out.println(a);
+        System.out.println(f);
+    }
+    
+}
